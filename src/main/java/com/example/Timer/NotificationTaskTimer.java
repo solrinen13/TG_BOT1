f85121f0d790f4334a01c8bb43af48a432ebc8a2
Построение изображения предмета в тонкой линзе <erhova.anastacia@yandex.ru>
2023-08-03 12:18:11 +0500

    1 commit

---
+++ src/main/java/com/example/Timer/NotificationTaskTimer.java
@@ -0,0 +1,39 @@
+package com.example.Timer;
+
+import com.example.repository.NotificationTaskRepository;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class NotificationTaskTimer {
+
+
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    private final TelegramBot telegramBot;
+
+    public NotificationTaskTimer(NotificationTaskRepository notificationTaskRepository, TelegramBot telegramBot) {
+        this.notificationTaskRepository = notificationTaskRepository;
+        this.telegramBot = telegramBot;
+    }
+
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    public void task() {
+        notificationTaskRepository.findAllByNotificationDateTime(
+                LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)
+        ).forEach(notificationTask -> {
+            telegramBot.execute(
+                    new SendMessage(notificationTask.getChatId(),
+                            "Напоминание:" + notificationTask.getMessage()));
+            notificationTaskRepository.delete(notificationTask);
+        });
+    }
+
+}
